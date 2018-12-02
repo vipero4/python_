00f0c638e1708e3e848ecd3864a6b7efefa08ca7
Artur Piórkowski <42784678+vipero4@users.noreply.github.com>
2018-12-02 14:24:31 +0100

    Create 7_list_coprehensions.py

---
+++ 7_list_coprehensions.py
@@ -0,0 +1,8 @@
+# Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
+# Write one line of Python that takes this list a and makes
+# a new list that has only the even elements of this list in it.
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+e = [n for n in a if n % 2 == 0]
+print(e)
