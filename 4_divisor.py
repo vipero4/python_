b2963fd1536d0043adc0029eb607e9bdf0600ef2
Artur Piórkowski <42784678+vipero4@users.noreply.github.com>
2018-12-02 14:22:32 +0100

    Update 4_divisor.py

--- 4_divisor.py
+++ 4_divisor.py
@@ -1,4 +1,3 @@
-
 # Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
 # (If you don’t know what a divisor is, it is a number that divides evenly into another number.
 # For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
@@ -8,6 +7,3 @@ x = range(1, number+1)
 for num in x:
     if number % num == 0:
         print(num)
-
-
-
