1857c0215ddd441b281b064d72a28bcbdfb7248b
Artur Piórkowski <42784678+vipero4@users.noreply.github.com>
2018-12-02 19:55:20 +0100

    Update 8_rock_paper_scissors.py

--- 8_rock_paper_scissors.py
+++ 8_rock_paper_scissors.py
@@ -1,6 +1,5 @@
 # Make a one player Rock-Paper-Scissors game.
-# (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner,
-# and ask if the players want to start a new game)
+# (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner)
 # Remember the rules:
 # Rock beats scissors
 # Scissors beats paper
