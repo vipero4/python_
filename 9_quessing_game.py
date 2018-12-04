eef600064daa187a145cef730c30aa369f8333b8
Artur Piórkowski <42784678+vipero4@users.noreply.github.com>
2018-12-04 21:43:29 +0100

    Update 9_quessing_game.py

--- 9_quessing_game.py
+++ 9_quessing_game.py
@@ -1,8 +1,6 @@
 # Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number,
 # then tell them whether they guessed too low, too high, or exactly right.
 # (Hint: remember to use the user input lessons from the very first exercise)
-
-
 # Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 import random
